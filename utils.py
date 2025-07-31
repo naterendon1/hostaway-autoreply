c44fb937847fff49bfbcc4d1cacb0f4af1e74aed
naterendon1 <naterendon@me.com>
2025-07-30 21:51:53 -0500

    Update utils.py

--- utils.py
+++ utils.py
@@ -7,8 +7,6 @@ import time
 from datetime import datetime
 from difflib import get_close_matches
 from datetime import datetime, timedelta
-from utils import fetch_hostaway_calendar, is_date_available
-
 
 # --- ENVIRONMENT VARIABLE CHECKS ---
 REQUIRED_ENV_VARS = [
